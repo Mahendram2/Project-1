43acca0867dd0d5165ed40f8145aad270978493d
Mahendra Mishra <Mahendra@Mahendras-MacBook-Pro.local>
2022-08-11 15:26:56 -0400

    WIP: sets IPO

---
+++ script.js
@@ -0,0 +1,11 @@
+/* IPO - Input Process Output */
+
+/* const - variable that never change/reassigned */
+
+/* State - varibale that do change  */
+
+/* cashed element refference */
+
+/* Event listener */
+
+/* Functions */
